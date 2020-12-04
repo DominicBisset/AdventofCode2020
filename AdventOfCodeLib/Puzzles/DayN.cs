e6009fc6e7aafbb7d3e59d8c00e528191506bdf6
Dominic Bisset <dominic.bisset@villagesoftware.co.uk>
2020-12-04 10:47:47 +0000

    Added easily-copiable file for new days.

---
+++ AdventOfCodeLib/Puzzles/DayN.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using AdventOfCode.Lib.Helpers;
+
+namespace AdventOfCode.Lib.Puzzles
+{
+    public class DayN : DaySolver<string, int>
+    {
+        public DayN(Data<string> data) : base(data)
+        {
+        }
+
+        public override int Puzzle1Solution()
+        {
+            return 0;
+        }
+
+        public override int Puzzle2Solution()
+        {
+            return 0;
+        }
+    }
+}
