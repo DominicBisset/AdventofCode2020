7c58187baadeb0c708fcb415cda72d7508254508
Dominic Bisset <dominic.bisset@villagesoftware.co.uk>
2020-12-05 15:49:05 +0000

    Resolved Part 2 Issue Answer was off by one, not sure why, but converting from ranges to regexs solved it

--- AdventOfCodeLib/Puzzles/Day4/StrictPassport.cs
+++ AdventOfCodeLib/Puzzles/Day4/StrictPassport.cs
@@ -10,10 +10,10 @@ namespace AdventOfCode.Lib.Puzzles.Day4
         internal override PassportField[] GetPassportDefinition()
         {
             return new PassportField[] {
-                new NumericRangePassportField(true, "byr","Birth Year",1920,2002),
-                new NumericRangePassportField(true, "iyr" ,"Issue Year",2010,2020),
-                new NumericRangePassportField(true, "eyr" ,"Expiration Year",2020,2030),
-                new RegExPassportField(true, "hgt" ,"Height", "^^1([5-8]\\d|9[0-3])cm|(59|6\\d|7[0-6])in$"),
+                new RegExPassportField(true, "byr","Birth Year", "^(19[2-9][0-9]|200[1-2])$"),
+                new RegExPassportField(true, "iyr" ,"Issue Year","^20(1\\d|20)$"),
+                new RegExPassportField(true, "eyr" ,"Expiration Year","^20(2\\d|30)$"),
+                new RegExPassportField(true, "hgt" ,"Height", "^1([5-8]\\d|9[0-3])cm|(59|6\\d|7[0-6])in$"),
                 new RegExPassportField(true, "hcl" ,"Hair Color", "^#[\\da-f]{6}$"),
                 new RegExPassportField(true, "ecl" ,"Eye Color", "^(amb|blu|brn|gry|grn|hzl|oth)$"),
                 new RegExPassportField(true, "pid" ,"Passport ID","^\\d{9}$"),
